7e4af84c874fe223534c492b4dc7c462c2443190
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-11-04 19:22:00 -0500

    small edits on package.json and config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   API_TOKEN: process.env.API_TOKEN || 'd3460822-1e38-11eb-adc1-0242ac120002',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:password@localhost/noteful',
+  DB_URL: process.env.DB_URL || 'https://quiet-plains-10352.herokuapp.com' ,
 };
