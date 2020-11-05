4ed457c38b2ec9b698a52ced43545800477700dc
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-11-05 11:25:47 -0500

    disable auth

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+//const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const validateBearerToken = require('./validate-bearer-token');
@@ -15,7 +15,7 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 }));
 
 
-app.use(cors());
+//app.use(cors());
 app.use(helmet());
 //app.use(validateBearerToken)
 
