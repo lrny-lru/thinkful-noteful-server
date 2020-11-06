c63a07f763c2fb56b8d62f01c05220e522e9f104
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-11-06 17:08:25 -0500

    yeah

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'd3460822-1e38-11eb-adc1-0242ac120002',
-  //DB_URL: process.env.DATABASE_URL || 'https://quiet-plains-10352.herokuapp.com' ,
+  //DATABASE_URL: process.env.DATABASE_URL || 'https://quiet-plains-10352.herokuapp.com' ,
   DATABASE_URL:process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful'
 
 };
