a071c1da5154c26916cb3465d5028ed7e13d02b8
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-11-06 16:35:09 -0500

    penguin

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-//const cors = require('cors');
+const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const validateBearerToken = require('./validate-bearer-token');
+//const validateBearerToken = require('./validate-bearer-token');
 const errorHandler = require('./error-handler');
 const notefulRouter = require('./router/noteful-router');
 
@@ -15,9 +15,9 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 }));
 
 
-//app.use(cors());
+app.use(cors());
 app.use(helmet());
-//app.use(validateBearerToken)
+
 
 app.use(notefulRouter);
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'production',
+  NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'd3460822-1e38-11eb-adc1-0242ac120002',
-  DB_URL: process.env.DB_URL || 'https://quiet-plains-10352.herokuapp.com' ,
+  //DB_URL: process.env.DATABASE_URL || 'https://quiet-plains-10352.herokuapp.com' ,
+  DATABASE_URL:process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful'
+
 };
